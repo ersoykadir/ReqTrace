7b04705bd5f8c2095627c7f94153da07d3ea254a
ersoykadir <eersooy61@gmail.com>
2023-11-27 01:43:36 +0300

    config for handling envs

---
+++ backend/src/server/config.py
@@ -0,0 +1,37 @@
+import os
+
+class Config:
+    """ 
+        Configuration class for the backend.
+        Contains environment variables and tracing parameters.
+    """
+
+    # environment variables
+    neo4j_username = os.environ.get('NEO4J_USERNAME')
+    neo4j_password = os.environ.get('NEO4J_PASSWORD')
+    neo4j_uri = os.environ.get('NEO4J_URI')
+    github_username = os.environ.get('GITHUB_USERNAME')
+    github_token = os.environ.get('GITHUB_TOKEN')
+
+    # Tracing parameters
+    # possible_search_methods = ['keyword', 'tf-idf', 'word-vector', 'llm-vector']
+    # search_method = 'keyword'
+    # parent_mode = False
+    # reset_graph = False
+    # experiment_mode = False
+    # model_setup = False
+    # filter_threshold = 0.5
+    # pretrained_model_path = os.environ.get('PRETRAINED_MODEL_PATH')
+
+    filter_date = '2023-08-01'
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = object.__new__(cls)
+            cls.instance.__initialized = False
+        return cls.instance
+
+    def __init__(self) -> None:
+        if self.__initialized:
+            return
+        self.__initialized = True
